e8733e1c415cf8ba77b2bbe2332bb56dc106788a
Michael Kupietz <26891995+kupietools@users.noreply.github.com>
2025-07-15 22:59:41 -0400

    Abstract popup close script steps to separate function, add some extra comments

--- js/script.js
+++ js/script.js
@@ -7,27 +7,34 @@
             e.preventDefault();
             $('.PLUGIN_PREFIX-panel').toggle();
         });
-        
-        $('.PLUGIN_PREFIX-close-button').on('click', function() {
-            $('.PLUGIN_PREFIX-panel').hide();
+    /* end Show/hide panel by clicking tab icon */
+    
+    /* hide panel by clicking panel close icon, if desired. Remove this section if you don't want that. */
+        $('.PLUGIN_PREFIX-close-button').on('click', function(e) {
+             e.preventDefault();
+             ktwp_closePanelScript(e);
         });
-    /* End show/hide panel by clicking tab icon. */ 
+    /* End panel by clicking panel close icon. */ 
     
     /* Hide panel by clicking outside it, if desired. Remove this section if you don't want that. */
-        $(document).on('click', function(event) {
+        $(document).on('click', function(e) {
             if (!$(event.target).closest('.PLUGIN_PREFIX-control').length && 
                 $('.PLUGIN_PREFIX-panel').is(':visible')) {
-                $('.PLUGIN_PREFIX-panel').hide();
+                ktwp_closePanelScript(e);
             }
         }); 
-  /* End hide panel by clicking outside it, if desired. */
+    /* End hide panel by clicking outside it, if desired. */
  
-  
-  /* Functionality for tab panel reset button, if desired. Remove this if you don't need that. */
+    /* Functionality for tab panel reset button, if desired. Remove this if you don't need that. */
         $('#PLUGIN_PREFIX-reset-button').on('click', function(e) {
           /* whatever the reset button should do goes here */
         });
-  /* End functionality for tab panel reset button */        
+    /* End functionality for tab panel reset button */        
+     
+     function ktwp_closePanelScript(e) { 
+    /* put any steps here that need to run when the panel closes */
+       $('.PLUGIN_PREFIX-panel').hide();
+     }
      
     });
 })(jQuery);
