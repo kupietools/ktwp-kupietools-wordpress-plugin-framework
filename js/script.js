3989225c3ed63c125b51672bb719607bee619334
Michael Kupietz <26891995+kupietools@users.noreply.github.com>
2025-07-21 01:11:23 -0400

    Update script.js for touchscreen okayness

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 (function() {
     'use strict';
 
+
+    function oncePanelOpen(panel) {/* put any steps here that have to be run whenever the panel is opened... whether by clicking a tab or displayed by a shortcode */
+};
+
     document.addEventListener('DOMContentLoaded', function() {
         /* Show/hide panel by clicking tab icon, if desired. Remove this section if you don't want that. */
         const icon = document.querySelector('.PLUGIN_PREFIX-icon');
@@ -9,11 +13,21 @@
                 e.preventDefault();
                 const panel = document.querySelector('.PLUGIN_PREFIX-panel');
                 if (panel) {
-                    panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
+                    panel.style.display = getComputedStyle(panel).display === 'none' ? 'block' : 'none';
                 }
+                const button = document.getElementById("ktwp_eyesHaveIt-control")
+				if (!panel.classList.contains("ktwp-de-beenDragged")) {
+					panel.style.top = parseInt(getComputedStyle(button).top)+20+"px";
+					 panel.style.left = "20px";}
+			    oncePanelOpen(panel);
             });
         }
         /* end Show/hide panel by clicking tab icon */
+        
+        /* make sure panel starts hidden... plugins such as KTWP Draggable Elements may make it visible at page load even if the css file in this plugin sets it to be hidden. You can remove this if you're not using the panel in this plugin*/
+        const panel = document.querySelector('.PLUGIN_PREFIX-panel');
+        if (panel) {panel.style.display="none !important";}
+        /* end make sure panel starts hidden */
 
         /* hide panel by clicking panel close icon, if desired. Remove this section if you don't want that. */
         const closeButton = document.querySelector('.PLUGIN_PREFIX-close-button');
@@ -31,7 +45,7 @@
             const panel = document.querySelector('.PLUGIN_PREFIX-panel');
 
             // Check if the click occurred outside the control area AND the panel is visible
-             if ( ((control  && !control.contains(e.target)) || !control) && panel && panel.style.display !== 'none') {
+             if ( ((control  && !control.contains(e.target)) || !control) && panel && getComputedStyle(panel).display !== 'none') {
                 ktwp_closePanelScript(e);
             }
         });
